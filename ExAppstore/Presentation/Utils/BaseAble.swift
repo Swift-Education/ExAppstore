811db338df96ecd1ac94d8679192b797baf2f68d
kangddong <rkdehddud96@naver.com>
2024-07-31 20:53:42 +0900

    default setup protocol + generic code

--- ExAppstore/Presentation/Utils/BaseAble.swift
+++ ExAppstore/Presentation/Utils/BaseAble.swift
@@ -15,5 +15,14 @@ extension UIViewController: BaseAble {}
 extension BaseAble where Self: UIViewController {
     func defaultSetup() {
         self.view.backgroundColor = .systemBackground
+        let label: UILabel = .init(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = String(describing: Self.self)
+        label.font = .systemFont(ofSize: 30)
+        self.view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        ])
     }
 }
